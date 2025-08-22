0a59d01919ab778f44a2f0011de89064569677fd
Jadon Zhu <jadonjzhu@gmail.com>
2025-08-21 17:13:41 -0700

    updated config and deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     // Your GitHub username and repository name
-    site: 'https://JadonJZhu.github.io',
-    base: '/enigma-website',
+    site: 'https://enigmaatucla.com',
   });
